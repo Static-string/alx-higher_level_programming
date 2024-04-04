5c4b284694fd58abcd276582ee37522c9e625356
Victor Adeyemo <billionaireteacher@gmail.com>
2024-04-03 18:09:16 -0700

    SOlved

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            c += 1
+        except IndexError:
+            break
+    print()
 
-class Square:
-    pass
-
-
-square = Square()
+    return c
