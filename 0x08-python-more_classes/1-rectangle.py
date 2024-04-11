839b851992831400f883bdc25c3ee42a772c71fe
Victor Adeyemo <billionaireteacher@gmail.com>
2024-04-10 17:10:28 -0700

    Fix task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,20 +7,15 @@ Default values of both attributes are 0.
 """
 
 
-
 class Rectangle:
     """A Rectangle class with attributes  width and height"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-
-
     @property
     def width(self):
         return self.__width
-
-
 
     @width.setter
     def width(self, value):
@@ -30,13 +25,9 @@ class Rectangle:
             raise ValueError('width must be >= 0')
         self.__width = value
 
-
-
     @property
     def height(self):
         return self.__height
-
-
 
     @height.setter
     def height(self, value):
